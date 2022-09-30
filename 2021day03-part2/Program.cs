9eb85a56768cda9b2dc0b4d419d712917c43e393
Robin Gude <guderobin@gmail.com>
2022-10-01 00:23:54 +0200

    day03-part2 finished

--- 2021day03-part2/Program.cs
+++ 2021day03-part2/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Diagnostics;
 using System.IO;
+using System.Diagnostics.Metrics;
 
 namespace _2021day03_part1
 {
@@ -12,17 +13,25 @@ namespace _2021day03_part1
         static void Main(string[] args)
         {
             //string[] sInputFile = File.ReadAllLines("C:\\Users\\rgude\\Desktop\\adventofcode\\day03\\input.txt"); Laptop
-            string[] sInputFile = File.ReadAllLines(C:\Users\guder\Source\Repos\robing29\adventofcode2021\inputs\day3.txt)
-            string[] sGammaOutput = new string[sInputFile[0].Length];
-            string[] sEpsilonOutput = new string[sInputFile[0].Length];
-            string sGammaFinal;
-            string sEpsilonFinal;
+            string[] sInputFile = File.ReadAllLines("C:\\Users\\guder\\Source\\Repos\\robing29\\adventofcode2021\\inputs\\day3.txt");
+            //life support rating = oxygen generator rating * C02 scrubber rating
+            //oxygen generator rating = immer der stärkere setzt sich durch
+            //C02 scrubber rating = immer der schwächere setzt sich durch
+            string[] sGammaOutput = new string[sInputFile[0].Length]; //mostCommon
+            string[] sEpsilonOutput = new string[sInputFile[0].Length]; //leastCommon
 
-            for (int i = 0; i < 12; i++)
+            string sOxygen;
+            string sC02;
+            List<string> lInput = sInputFile.ToList<string>();
+            List<string> lC02 = sInputFile.ToList<string>();
+
+            int i = 0;
+            while (lInput.Count != 1)
             {
+
                 int iCounter1 = 0;
                 int iCounter0 = 0;
-                foreach (string s in sInputFile)
+                foreach (string s in lInput)
                 {
                     if (s[i] == '1')
                     {
@@ -32,35 +41,60 @@ namespace _2021day03_part1
                     {
                         iCounter0++;
                     }
-                }
-                Console.WriteLine("iCounter1 für i = " + i + " ist " + iCounter1);
-                Console.WriteLine("iCounter0 für i = " + i + " ist " + iCounter0);
+                } //Bits zählen
+
                 if (iCounter1 >= iCounter0)
                 {
-                    Console.WriteLine("Die " + i + ". Stelle von vorne bei sGammaOutput ist 1");
-                    Console.WriteLine("Die " + i + ". Stelle von vorne bei sEpsilonOutput ist 0");
-                    sGammaOutput[i] = "1";
-                    sEpsilonOutput[i] = "0";
-                }
+                    lInput.RemoveAll(s => s[i] == '0');
+                } //Remove from list
                 else
                 {
-                    Console.WriteLine("Die " + i + ". Stelle von vorne bei sGammaOutput ist 0");
-                    Console.WriteLine("Die " + i + ". Stelle von vorne bei sEpsilonOutput ist 1");
-                    sGammaOutput[i] = "0";
-                    sEpsilonOutput[i] = "1";
-                }
-
-
+                    lInput.RemoveAll(s => s[i] == '1');
+                } //Remove from list
+                i++;
             }
-            sGammaFinal = string.Join(null, sGammaOutput);
-            sEpsilonFinal = string.Join(null, sEpsilonOutput);
-            Console.WriteLine("sGammaFinal = " + sGammaFinal);
-            Console.WriteLine("sEpsilonFinal = " + sEpsilonFinal);
+            sOxygen = lInput.First();
+            Console.WriteLine("Oxygen = " + sOxygen);
 
-            long iGamma = Convert.ToInt64(sGammaFinal, 2);
-            long iEpsilon = Convert.ToInt64(sEpsilonFinal, 2);
-            Console.WriteLine("iGamma = " + iGamma + " und iEpsilon = " + iEpsilon);
-            Console.WriteLine("Power Current = " + iGamma * iEpsilon);
+            int i2 = 0;
+            while (lC02.Count != 1)
+            {
+                int iCounter1 = 0;
+                int iCounter0 = 0;
+                foreach (string s in lC02)
+                {
+                    if (s[i2] == '1')
+                    {
+                        iCounter1++;
+                    }
+                    else
+                    {
+                        iCounter0++;
+                    }
+                } //Bits zählen
+
+                if (iCounter1 >= iCounter0)
+                {
+                    lC02.RemoveAll(s => s[i2] == '1');
+                } //Remove from list
+                else
+                {
+                    lC02.RemoveAll(s => s[i2] == '0');
+                } //Remove from list
+                i2++;
+            }
+
+            sC02 = lC02.First();
+            Console.WriteLine("C02 = " + sC02);
+
+            long longC02 = Convert.ToInt64(sC02, 2);
+            long longOxygen = Convert.ToInt64(sOxygen, 2);
+
+            Console.WriteLine("longC02 = " + longC02);
+            Console.WriteLine("longOxygen = " + longOxygen);
+            Console.WriteLine("Life Support Rating = " + longC02 * longOxygen);
+
+            //https://www.youtube.com/watch?v=tmzpgfldSE4
         }
     }
 }
