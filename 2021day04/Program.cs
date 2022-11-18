c90d760c8b7efabd03cdd50d3e7cc4a122b71f14
Robin Gude <guderobin@gmail.com>
2022-11-18 17:13:57 +0100

    - Added day04

---
+++ 2021day04/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Diagnostics.Metrics;
+using System.Data;
+
+namespace _2021day03_part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] sInputFile = File.ReadAllLines("C:\\Users\\rgude\\Desktop\\adventofcode\\day03\\input.txt"); Laptop
+            //string[] sInputFile = File.ReadAllLines("C:\\Users\\guder\\Source\\Repos\\robing29\\adventofcode2021\\inputs\\day3.txt");
+            
+            //Initialisation
+
+            string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string sFile = System.IO.Path.Combine(sCurrentDirectory, @"..\..\..\..\inputs\day4.txt");
+            string sFilePath = Path.GetFullPath(sFile);
+            string[] sInputFile = File.ReadAllLines(sFilePath);
+            List<string> AllNumbers = new List<string>();
+            List<string> NumbersDrawn1 = new List<string>();
+            List<string> BingoBoards = new List<string>();
+            AllNumbers = File.ReadLines(sFilePath).ToList();
+            NumbersDrawn1 = AllNumbers[0].Split(',').ToList();
+            AllNumbers.RemoveAt(0);
+            BingoBoards = AllNumbers;
+            BingoBoards.RemoveAll(x => x == "");
+
+            DataTable dBingoBoard = new DataTable();
+
+            dBingoBoard.CaseSensitive = false;
+            dBingoBoard
+            
+            //var FirstOccurence = AllNumbers.Find("\\n");
+            foreach (string s in BingoBoards)
+            {
+                Console.WriteLine(s);
+            }
+           
+        }
+    }
+}
