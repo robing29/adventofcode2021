5d964a1f106ad77826b70acaf5e6002702f3595d
Robin Gude <guderobin@gmail.com>
2023-05-05 18:15:31 +0200

    day5 part1 finished. part2 in progress

---
+++ aoc2021-day5/Program.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+///<summary>Data Input</summary>
+///
+var input = File.ReadAllLines(@"..\..\..\..\inputs\day5.txt").ToList();
+List<string> inputPart2 = new List<string>(input);
+//File.ReadAllLines(@"..\..\..\..\inputs\day4.txt").ToList();
+List<int[]> punkteViererpacksListe = new List<int[]>();
+List<int[]> punktePaareListe = new List<int[]>();
+
+///<summary>Part 1</summary>
+///
+foreach (string zeile in input)
+{
+    var zeileWerte = zeile.Split(" -> ");
+    var zeileErsteKoordinaten = zeileWerte[0].Split(",");
+    int x1 = int.Parse(zeileErsteKoordinaten[0]);
+    int y1 = int.Parse(zeileErsteKoordinaten[1]);
+    var zeileZweiteKoordinaten = zeileWerte[1].Split(",");
+    int x2 = int.Parse(zeileZweiteKoordinaten[0]);
+    int y2 = int.Parse(zeileZweiteKoordinaten[1]);
+
+    if (x1 == x2 || y1 == y2)
+    {
+        int[] punktePaare = { x1, y1,x2,y2};
+        punkteViererpacksListe.Add(punktePaare);
+    }
+}
+
+foreach (var vierPunkteWerte in punkteViererpacksListe)
+{
+    //x1 = 10, y1= 50, x2 = 10, y2 = 30
+    //x1 = 30, y1=10, x2 = 50, y2= 10
+    if (vierPunkteWerte[0] == vierPunkteWerte[2]) //x-Werte gleich
+    {
+        if (vierPunkteWerte[1] >= vierPunkteWerte[3]) //erster Y-Wert größer als der zweite
+        {
+            for (int i = vierPunkteWerte[3]; i <= vierPunkteWerte[1]; i++)
+            {
+                int[] punktePaar = { vierPunkteWerte[0], i };
+                punktePaareListe.Add(punktePaar);
+            }
+        }
+        else
+        {
+            for (int i = vierPunkteWerte[1]; i <= vierPunkteWerte[3]; i++)
+            {
+                int[] punktePaar = { vierPunkteWerte[0], i };
+                punktePaareListe.Add(punktePaar);
+            }
+        }
+        
+    } else if (vierPunkteWerte[1] == vierPunkteWerte[3]) //Y-Werte gleich
+    {
+        if (vierPunkteWerte[0] >= vierPunkteWerte[2]) //erster Y-Wert größer als der zweite
+        {
+            for (int i = vierPunkteWerte[2]; i <= vierPunkteWerte[0]; i++)
+            {
+                int[] punktePaar = { i, vierPunkteWerte[1] };
+                punktePaareListe.Add(punktePaar);
+            }
+        }
+        else
+        {
+            for (int i = vierPunkteWerte[0]; i <= vierPunkteWerte[2]; i++)
+            {
+                int[] punktePaar = { i, vierPunkteWerte[1] };
+                punktePaareListe.Add(punktePaar);
+            }
+        }
+    }
+}
+
+int numberOfDuplicateGroups = punktePaareListe
+    .GroupBy(array => string.Join(",", array))
+    .Count(group => group.Count() > 1);
+
+Console.WriteLine(numberOfDuplicateGroups);
+
+///<summary>Part 2</summary>
+///
+
+// punkteViererpacksListe = x1, y1, x2, y2;
+
+List<int[]> punktePaareListePart2 = new List<int[]>();
+int[] punktePaarPart2 = new int[2];
+foreach (var intArray in punkteViererpacksListe)
+{
+    int maxX = Math.Max(intArray[0], intArray[2]); //max X-Value
+    int minX = Math.Min(intArray[0], intArray[2]); //min X-Value
+    int maxY = Math.Max(intArray[1], intArray[3]); //max Y-Value
+    int minY = Math.Min(intArray[1], intArray[3]); //min Y-Value
+
+    if (intArray[0] == minX && intArray[1] == minY)
+    {
+        for (int i = intArray[0]; i <= intArray[2]; i++)
+        for (int k = intArray[1]; k <= intArray[3]; k++)
+            {
+                punktePaarPart2[0] = i;
+                punktePaarPart2[1] = k;
+                punktePaareListePart2.Add(punktePaarPart2);
+            }
+
+    }
+    if (intArray[0] == minX && intArray[1] == maxY)
+    {
+        for (int i = intArray[0]; i <= intArray[2]; i++)
+        for (int k = intArray[1]; k >= intArray[3]; k--)
+            {
+                punktePaarPart2[0] = i;
+                punktePaarPart2[1] = k;
+                punktePaareListePart2.Add(punktePaarPart2);
+            }
+    }
+    if (intArray[0] == maxX && intArray[1] == maxY)
+    {
+        for (int i = intArray[0]; i >= intArray[2]; i--)
+            for (int k = intArray[1]; k >= intArray[3]; k--)
+            {
+                punktePaarPart2[0] = i;
+                punktePaarPart2[1] = k;
+                punktePaareListePart2.Add(punktePaarPart2);
+            }
+    }
+    if (intArray[0] == maxX && intArray[1] == minY)
+    {
+        for (int i = intArray[0]; i >= intArray[2]; i--)
+            for (int k = intArray[1]; k <= intArray[3]; k++)
+            {
+                punktePaarPart2[0] = i;
+                punktePaarPart2[1] = k;
+                punktePaareListePart2.Add(punktePaarPart2);
+            }
+    }
+    foreach (var item in punktePaareListePart2)
+    {
+        Console.WriteLine($"{string.Join(",", item)}");
+    }
+}
+
---
+++ linq-uebungen/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+//List<int> numbers = new List<int> { 8, 2, 6, 4, 9, 1, 5, 3, 7 };
+
+//var groupedNumbers = numbers.GroupBy(n => n % 2 == 0 ? "Even" : "Odd");
+
+//Console.WriteLine(groupedNumbers);
+
+using linq_uebungen;
+using System.Threading.Tasks.Dataflow;
+
+bool isPrime(int number)
+{
+    if (number <= 1) return false;
+    if (number == 2) return true;
+    if (number % 2 == 0) return false;
+
+    var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+    for (int i = 3; i <= boundary; i += 2)
+    {
+        if (number % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+List<int> numbers = new List<int> { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
+
+var primeNumbers = numbers.Where(x => isPrime(x) == true).ToList();
+
+primeNumbers.ForEach(x => Console.WriteLine(x));
+
+List<string> fruits = new List<string> { "apple", "banana", "orange", "grape", "strawberry", "pineapple" };
+
+var bigFruits = fruits.Where(x => x.Length >= 6).Select(x => x.ToUpper()).ToList();
+
+bigFruits.ForEach(x => Console.WriteLine(x));
+
+List<Student> students = new List<Student>
+{
+    new Student {Name = "Alice", Age = 23 },
+    new Student {Name = "Bob", Age = 25 },
+    new Student {Name = "Charlie", Age = 23 },
+    new Student {Name = "David", Age = 28 },
+    new Student {Name = "Eva", Age = 25 }
+};
+
+var groups = students.GroupBy(s => s.Age);
+
+foreach (var group in groups)
+{
+    Console.WriteLine($"{ group.Key} Count: {group.Count()}");
+    Console.WriteLine($"Students: {string.Join(", ", group.Select(s => s.Name))}");
+    Console.WriteLine();
+}
