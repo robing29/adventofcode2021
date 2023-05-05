9e92bea388174415f1b428e83d5a61646572faa9
Robin Gude <guderobin@gmail.com>
2023-05-05 23:30:32 +0200

    structs sind zwar schneller, aber immer noch sehr langsam. Commit, bevor ich es mit ints probiere

--- 2021day06/Program.cs
+++ 2021day06/Program.cs
@@ -7,48 +7,91 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //var input = File.ReadAllText(@"..\..\..\..\inputs\day6.txt");
-        string input = "3,4,3,1,2";
+        var input = File.ReadAllText(@"..\..\..\..\inputs\day6.txt");
+        //string input = "3,4,3,1,2";
         List<int> list = new List<int>();
         list = input.Split(",").ToList().Select(x => int.Parse(x)).ToList();
 
         List<Fish> fishInventory = new List<Fish>();
+        List<FishStruct> fishStructs = new List<FishStruct>();
         foreach (int i in list)
         {
-            fishInventory.Add(new Fish(i));
+            //fishInventory.Add(new Fish(i));
+            fishStructs.Add(new FishStruct(i));
         }
 
         List<Fish> newFish = new List<Fish>();
+        List<FishStruct> newFishStructs = new List<FishStruct>();
         Stopwatch sw = new Stopwatch();
         int afterDays = 256;
         for (int i = 0; i < afterDays; i++)
         {
             sw.Start();
-            foreach (Fish fish in fishInventory)
+            for (int k = 0; k < fishStructs.Count; k++)
             {
+                FishStruct fish = fishStructs[k];
                 if (fish.DaysUntilReset == 0)
                 {
-                    newFish.Add(fish.ResetAndBirth());
+                    newFishStructs.Add(fish.ResetAndBirth());
+                    fish.DaysUntilReset = 6;
+                    fishStructs[k] = fish;
                 }
                 else
                 {
                     fish.DaysUntilReset--;
+                    fishStructs[k] = fish;
                 }
             }
-            newFish.ForEach(x => fishInventory.Add(x));
-            newFish.Clear();
-            Console.WriteLine($"day{i}-Dauer: {sw.Elapsed}");
+            newFishStructs.ForEach(x => fishStructs.Add(x));
+            newFishStructs.Clear();
+            //newFish.ForEach(x => fishInventory.Add(x));
+            //newFish.Clear();
+            Console.WriteLine($"day{i + 1}-Dauer: {sw.Elapsed}");
         }
 
+        //for (int i = 0; i < afterDays; i++)
+        //{
+        //    sw.Start();
+        //    foreach (Fish fish in fishInventory)
+        //    {
+        //        if (fish.DaysUntilReset == 0)
+        //        {
+        //            newFish.Add(fish.ResetAndBirth());
+        //        }
+        //        else
+        //        {
+        //            fish.DaysUntilReset--;
+        //        }
+        //    }
+        //    newFish.ForEach(x => fishInventory.Add(x));
+        //    newFish.Clear();
+        //    Console.WriteLine($"day{i+1}-Dauer: {sw.Elapsed}");
+        //}
+        //Dauer bei afterDays = 200 mit classes: 00:00:45.9082687
+        //zweiter Benchmark: day200-Dauer: 00:00:46.6338716
 
 
-        Console.WriteLine(fishInventory.Count);
+        //Console.WriteLine(fishInventory.Count);
+        //Console.WriteLine(fishStructs.Count);
         Console.WriteLine("done");
         Console.ReadLine();
     }
 }
 
+public struct FishStruct
+{
+    public FishStruct(int anfangsFisch)
+    {
+        DaysUntilReset = anfangsFisch;
+    }
 
+    public int DaysUntilReset { get; set; }
+
+    public FishStruct ResetAndBirth()
+    {
+        return new FishStruct(8);
+    }
+}
 
 public class Fish
 {
