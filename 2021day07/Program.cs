bddfcfef640df79052d0d6da45a88212b515ce3c
Robin Gude <guderobin@gmail.com>
2023-05-07 23:08:11 +0200

    Begun day7

---
+++ 2021day07/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //var input = File.ReadAllText(@"..\..\..\..\inputs\day6.txt");
+        string input = "16,1,2,0,4,2,7,1,2";
+
+        var intArray = input.Split(",").Select(int.Parse).ToList();
+        intArray.Sort();
+
+
+        int median;
+        if (intArray.Count % 2 != 0)
+        {
+            median = intArray[intArray.Count/2];
+        } else
+        {
+            median = (intArray[intArray.Count/2]+intArray[(intArray.Count/2)-1])/2;
+        }
+
+        intArray.ForEach(i => Console.WriteLine(i));
+        
+        Console.WriteLine(median);
+        Console.WriteLine("Hello, World!");
+    }
+}
